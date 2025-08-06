73581c099c9f9f574037a62d2fbec8951ae7d8cf
Rod-1502 <rodrigo_liba@ig.com.br>
2025-08-05 21:45:25 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,20 @@
+let heroi = "Elrod"
+let xp =5200
+
+if (xp <= 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro";  
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";  
+} else {
+  nivel = "lendário";
+}
+
+console.log("O herói de nome " + heroi + " está no nível de " + nivel)
